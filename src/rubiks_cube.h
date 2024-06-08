a346aa96cf3858d2cd06780382050930e95a151a
idiasdas <igordias.ti@gmail.com>
2024-06-08 12:45:52 +0200

    [BUG-FIX] Fix faces' indices definitions

--- src/rubiks_cube.h
+++ src/rubiks_cube.h
@@ -25,3 +25,10 @@
 #define X_AXIS {1.0f, 0.0f, 0.0f}
 #define Y_AXIS {0.0f, 1.0f, 0.0f}
 #define Z_AXIS {0.0f, 0.0f, 1.0f}
+
+#define RIGHT_FACE  {2, 5, 8, 11, 14, 17, 20, 23, 26}
+#define LEFT_FACE   {6, 3, 0, 15, 12, 9, 24, 21, 18}
+#define TOP_FACE    { 6, 7, 8, 3, 4, 5, 0, 1, 2}
+#define BOTTOM_FACE {18, 19, 20, 21, 22, 23, 24, 25, 26}
+#define FRONT_FACE  {0, 1, 2, 9, 10, 11, 18, 19, 20}
+#define BACK_FACE   {8, 7, 6, 17, 16, 15, 26, 25, 24}
